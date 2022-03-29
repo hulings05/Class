4eb1dedce56789d2ab088567eecda822fef4af45
hulings05 <97485236+hulings05@users.noreply.github.com>
2022-03-28 23:03:00 -0400

    Add files via upload

---
+++ Car Class.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace Class {
+
+ public class Car {
+public void carnoise()
+    {
+        Console.WriteLine("vroom vroom");
+    }
+    public void Speed()
+    {
+        Console.WriteLine(" - Max speed is 160");
+    }
+    public void Doors()
+    {
+        Console.WriteLine(" - 4 doors");
+    }
+   public void MPG()
+    {
+        Console.WriteLine(" - MPG is 30");
+    }
+  }
+
+    public class Truck {
+ public void trucknoise()
+ {
+        Console.WriteLine("purr purr");
+    }
+    public void Speed()
+    {
+        Console.WriteLine(" - Max speed is 120");
+    }
+     public void Doors()
+    {
+        Console.WriteLine(" - 2 doors");
+    }
+   public void MPG()
+    {
+        Console.WriteLine(" - MPG is 14");
+    }
+  }
+
+  public class Van {
+ public void vannoise()
+ {
+        Console.WriteLine("zoom zoom");
+    }
+    public void Speed()
+    {
+        Console.WriteLine(" - Max speed is 140");
+    }
+    public void Doors()
+    {
+        Console.WriteLine(" - 2 doors");
+    }
+   public void MPG()
+    {
+        Console.WriteLine(" - MPG is 20");
+    }
+  }
